9f814a2e05507129f78166c3b1d91bb6de699bb6
foochane <fuchengkm@qq.com>
2020-02-12 10:13:21 +0800

    第3天 服务端接口开发

--- xcEduService/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xcEduService/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -13,7 +13,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
-@ComponentScan(basePackages={"com.xuecheng.manage_cms"})//扫描本项目下的所有类,不加也可以，ManageCmsApplication会自动扫描所在包下的类
+@ComponentScan(basePackages={"com.xuecheng.framework"})//扫描common包下的类
+@ComponentScan(basePackages={"com.xuecheng.manage_cms"})//扫描本项目下的所有类
 public class ManageCmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ManageCmsApplication.class,args);
